9efea6079d772781cb21fcbcc53d7ba24c2f02f2
Ahmed Ali <ahmed.alawadly@gmail.com>
2021-03-22 20:00:03 +0200

    fix ch08 p09 solution

--- Chapter08/9-parens.py
+++ Chapter08/9-parens.py
@@ -1,16 +1,19 @@
 from typing import List
 
 def generateParens(n: int) -> List[str]:
-  if n == 1:
-    return ['()']
-
-  parens = generateParens(n-1)
   res = []
-  for p in parens:
-    res.append(p +'()')
-    if p + '()' != '()' + p:
-      res.append('()' + p)
-    res.append('(' + p + ')')
+  backtrack('', 0, 0, res, n)
   return res
+
+
+def backtrack(s: str, left: int, right: int, res: List[str], n: int):
+  if len(s) == 2 * n:
+    res.append(s)
+
+  if left < n:
+    backtrack(s + '(', left + 1, right, res, n)
+  if right < left:
+    backtrack(s + ')', left, right + 1, res, n)
+
 
 print(generateParens(3))
